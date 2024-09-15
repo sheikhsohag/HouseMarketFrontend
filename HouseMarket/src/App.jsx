c76ee1d271dda76c406ec969bc382c9e485d5e9d
sheikhsohag <sheikhsohag000@gmail.com>
2024-09-15 10:23:14 +0600

    HouseMarket Frontend basic project created

---
+++ HouseMarket/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Container>
+        
+      </Container>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
